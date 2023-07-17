5e1d37b4197c26acafd494e8e90133c958220e05
tech-911 <babatundeolatunji74@gmail.com>
2023-07-17 11:37:49 +0100

    ...created signin page

---
+++ src/app/(authentication)/login/layout.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login",
+}
+
+export default function RegistrationLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <div className="relative h-screen bg-gradient-body font-raleway">{children}</div>
+}
