cbba22442e9843f39ca0eb5d3270d9d97e0279f8
Yahya Ahmad <yahay.me@gmail.com>
2024-01-12 05:36:34 -0800

    fixes

--- src/app/(business-dashboard)/dashboard/registered-business/components/approval-description.tsx
+++ src/app/(business-dashboard)/dashboard/registered-business/components/approval-description.tsx
@@ -8,12 +8,13 @@ import InformationFrame from "./information-frame";
 import PersonalInformation from "./personal-information";
 import BusinessInformation from "./business-information";
 import AccountInformation from "./account-information";
+import Link from "next/link";
 type Props = {};
 
 export default function ApprovalDescription(props: any) {
   return (
     <div>
-      <ReviewDetails props={props}/>
+      <ReviewDetails props={props} />
 
       {/* form display section */}
       <div className=" w-full inline-flex flex-col items-start justify-start gap-6 px-10 pt-6 pb-10 bg-white border rounded-lg border-slate-200">
@@ -22,9 +23,12 @@ export default function ApprovalDescription(props: any) {
             Review Information
           </Typography>
 
-          {/* <Button className="text-center text-white text-sm font-bold font-['Century Gothic'] self-stretch px-8 py-2.5 bg-sky-400 rounded justify-center items-center inline-flex">
-            Edit records
-          </Button> */}
+          <Button className="text-center text-white text-sm font-bold font-['Century Gothic'] self-stretch px-8 py-2.5 bg-sky-400 rounded justify-center items-center inline-flex">
+            <Link key={"/"} href='/dashboard'>
+              Done
+            </Link>
+
+          </Button>
         </div>
         <div className="self-stretch h-px border border-gray-200"></div>
         <div className="flex flex-col items-start justify-start space-y-3 ">
